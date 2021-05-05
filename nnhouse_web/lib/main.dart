1e0edb99b64878ccef71ca3ef4c338781ea4a9b9
nnbinh <nnbinh0301@gmail.com>
2021-05-05 12:58:29 +0700

    add fire store. https://firebase.flutter.dev/docs/firestore/usage https://www.youtube.com/watch?v=Ue_dIKOMcb4

--- nnhouse_web/lib/main.dart
+++ nnhouse_web/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,19 +22,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization, builder: (context, snapshot) {
+          if(snapshot.hasError) return somethingWentWrong(context, snapshot.error.toString());
+          if(snapshot.connectionState == ConnectionState.done) return getAppBody(context);
+          return loading(context);
+    });
+  }
+
+  Widget somethingWentWrong(BuildContext context, String msg) {
+    return Text(msg, style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.red));
+  }
+
+  Widget loading(BuildContext context) {
+    return CircularProgressIndicator();
+  }
+
+  Widget getAppBody(BuildContext context) {
     return MaterialApp(
       title: appName,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        accentColor: kAccentColor,
-        brightness: Brightness.light,
-        fontFamily: GoogleFonts.openSans().fontFamily,
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor, displayColor: kAccentColor)
-      ),
+          primaryColor: kPrimaryColor,
+          accentColor: kAccentColor,
+          brightness: Brightness.light,
+          fontFamily: GoogleFonts.openSans().fontFamily,
+          textTheme: Theme.of(context)
+              .textTheme
+              .apply(bodyColor: kTextColor, displayColor: kAccentColor)),
       home: HomeScreen(),
     );
   }
